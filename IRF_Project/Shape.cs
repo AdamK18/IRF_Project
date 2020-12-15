4f48bd8fee3695b35958cd95ee770dee37a7ebcf
k.Adam <kloczl.adam@gmail.com>
2020-12-15 22:43:45 +0100

    added data and shape class

---
+++ IRF_Project/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project
+{
+    class Shape : Button
+    {
+        private string name;
+        private string[][] data;
+        private int dimensions;
+
+        public Shape(string name, string[][] data, int dimensions)
+        {
+            this.name = name;
+            this.data = data;
+            this.dimensions = dimensions;
+        }
+    }
+}
